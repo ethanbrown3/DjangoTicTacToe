970283edb817408f45b472e22ed9561c4dd3ae0f
Ethan Brown <ethanbrown3@mail.weber.edu>
2018-10-25 21:55:48 -0600

    admin view and models updated

--- tictactoe/gameplay/models.py
+++ tictactoe/gameplay/models.py
@@ -4,12 +4,17 @@ from django.contrib.auth.models import User
 
 class Game(models.Model):
     first_player = models.ForeignKey(User,
-                                     related_name="games_first_player")
+                                     related_name="games_first_player",
+                                     on_delete=models.CASCADE)
     second_player = models.ForeignKey(User,
-                                      related_name="games_second_player")
+                                      related_name="games_second_player",
+                                      on_delete=models.CASCADE)
     start_time = models.DateTimeField(auto_now_add=True)
     last_active = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=1, default='F')
 
+    def __str__(self):
+        return f"{self.first_player} vs {self.second_player}"
 
 class Move(models.Model):
     x = models.IntegerField()
