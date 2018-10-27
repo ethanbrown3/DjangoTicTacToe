8e3b4dde0e07d30799d85f666794e68da3dee204
Ethan Brown <ethanbrown3@mail.weber.edu>
2018-10-27 09:10:59 -0600

    Added user's current games to player/home. Added custom QS to games model

--- tictactoe/gameplay/models.py
+++ tictactoe/gameplay/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models import Q
 from django.contrib.auth.models import User
 
 GAME_STATUS_CHOICES = (
@@ -8,6 +9,19 @@ GAME_STATUS_CHOICES = (
     ('L', 'Second Player Wins'),
     ('D', 'Draw')
 )
+
+
+class GamesQuerySet(models.QuerySet):
+    def games_for_user(self, user):
+
+        return self.filter(
+            Q(first_player=user) |  Q(second_player=user)
+        )
+
+    def active(self):
+        return self.filter((
+            Q(status='F') | Q(status='S')
+        ))
 
 
 class Game(models.Model):
@@ -20,6 +34,8 @@ class Game(models.Model):
     start_time = models.DateTimeField(auto_now_add=True)
     last_active = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=1, default='F', choices=GAME_STATUS_CHOICES)
+
+    objects = GamesQuerySet.as_manager()
 
     def __str__(self):
         return f"{self.first_player} vs {self.second_player}"
--- tictactoe/player/views.py
+++ tictactoe/player/views.py
@@ -9,5 +9,8 @@ def home(request):
     :param request:
     :return:
     """
+    my_games = Game.objects.games_for_user(request.user)
+    active_games = my_games.active()
+
     return render(request, "player/home.html",
-                  {'ngames': Game.objects.count()})
+                  {'games': active_games})
