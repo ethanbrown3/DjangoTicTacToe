1fe03b1b4edd82017fc3ff24b9a14a7adbab78bc
Ethan Brown <ethanbrown3@mail.weber.edu>
2018-10-29 23:45:03 -0600

    logic for submitting invitation form

--- tictactoe/player/views.py
+++ tictactoe/player/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 
 from .forms import InvitationForm
+from .models import Invitation
 from gameplay.models import Game
 
 
@@ -18,7 +19,16 @@ def home(request):
     return render(request, "player/home.html",
                   {'games': active_games})
 
+
 @login_required
 def new_invitation(request):
-    form = InvitationForm()
+    if request.method == 'POST':
+        # validation
+        invitation = Invitation(from_user=request.user)
+        form = InvitationForm(instance=invitation, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('player_home')
+    else:
+        form = InvitationForm()
     return render(request, 'player/new_invitation_form.html', {'form': form})
