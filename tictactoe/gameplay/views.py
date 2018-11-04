03cee5e73aeeafa2be329512bcfc97a7588690b7
Ethan Brown <ethanbrown3@mail.weber.edu>
2018-11-03 21:13:03 -0600

    Added move form

--- tictactoe/gameplay/views.py
+++ tictactoe/gameplay/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 
+from .forms import MoveForm
 from .models import Game
+
 
 @login_required
 def game_detail(request, id):
     game = get_object_or_404(Game, pk=id)
+    context = {'game': game}
+    if game.is_users_move(request.user):
+        context['form'] = MoveForm()
     return render(
         request,
         'gameplay/game_detail.html',
-        {'game': game}
+        context
     )
