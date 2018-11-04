3cef0195f69ffac01b16149716d421b13d906889
Ethan Brown <ethanbrown3@mail.weber.edu>
2018-11-03 21:46:53 -0600

    Added win and draw logic

--- tictactoe/gameplay/models.py
+++ tictactoe/gameplay/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.db.models import Q
 from django.contrib.auth.models import User
 from django.urls import reverse
+from django.core.validators import MinValueValidator, MaxValueValidator
+
 
 GAME_STATUS_CHOICES = (
     ('F', 'First Player To Move'),
@@ -11,6 +13,7 @@ GAME_STATUS_CHOICES = (
     ('D', 'Draw')
 )
 BOARD_SIZE = 3
+
 
 class GamesQuerySet(models.QuerySet):
     def games_for_user(self, user):
@@ -51,6 +54,33 @@ class Game(models.Model):
         return (user == self.first_player and self.status == 'F') or\
             (user == self.second_player and self.status == 'S')
 
+    def new_move(self):
+        """Returns a new move object with player, game and count preset
+        """
+        if self.status not in 'FS':
+            raise ValueError("Cannot make move on finished game")
+
+        return Move(
+            game=self,
+            by_first_player=self.status == 'F'
+        )
+
+    def update_after_move(self, move):
+        """Update the status of the game, given the last move."""
+        self.status = self._get_game_status_after_move(move)
+
+    def _get_game_status_after_move(self, move):
+        x, y = move.x, move.y
+        board = self.board()
+        if (move == board[y][0] == board[y][1] == board[y][2]) or \
+           (move == board[0][x] == board[1][x] == board[2][x]) or \
+           (move == board[0][0] == board[1][1] == board[2][2]) or \
+           (move == board[0][2] == board[1][1] == board[2][0]):
+            return "W" if move.by_first_player else "L"
+        if self.move_set.count() >= BOARD_SIZE**2:
+            return 'D'
+        return 'S' if self.status == 'F' else 'F'
+
     def get_absolute_url(self):
         return reverse('gameplay_detail', args=[self.id])
 
@@ -59,9 +89,24 @@ class Game(models.Model):
 
 
 class Move(models.Model):
-    x = models.IntegerField()
-    y = models.IntegerField()
+    x = models.IntegerField(
+        validators=[MinValueValidator(0),
+                    MaxValueValidator(BOARD_SIZE-1)]
+    )
+    y = models.IntegerField(
+        validators=[MinValueValidator(0),
+                    MaxValueValidator(BOARD_SIZE-1)]
+    )
     comment = models.CharField(max_length=300, blank=True)
-    game=models.ForeignKey(Game, on_delete=models.CASCADE)
+    game = models.ForeignKey(Game, editable=False, on_delete=models.CASCADE)
     by_first_player = models.BooleanField(editable=False)
 
+    def __eq__(self, other):
+        if other is None:
+            return False
+        return other.by_first_player == self.by_first_player
+
+    def save(self, *args, **kwargs):
+        super(Move, self).save(*args, **kwargs)
+        self.game.update_after_move(self)
+        self.game.save()
--- tictactoe/gameplay/urls.py
+++ tictactoe/gameplay/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path, re_path
 from django.contrib.auth.views import LoginView, LogoutView
 
-from .views import game_detail
+from .views import game_detail, make_move
 
 urlpatterns = [
     re_path(r'detail/(?P<id>\d+)/$',
             game_detail,
-            name='gameplay_detail')
+            name='gameplay_detail'),
+    re_path(r'make_move/(?P<id>\d+)/',
+            make_move,
+            name='gameplay_make_move')
 ]
 
