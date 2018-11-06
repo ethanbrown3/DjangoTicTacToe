24f396ddd9fa047335b3ead2461b1ca86f59b2ae
Ethan Brown <ethanbrown3@mail.weber.edu>
2018-11-05 20:07:42 -0700

    added games list view

--- tictactoe/gameplay/urls.py
+++ tictactoe/gameplay/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, re_path
 from django.contrib.auth.views import LoginView, LogoutView
 
-from .views import game_detail, make_move
+from .views import game_detail, make_move, AllGamesList
 
 urlpatterns = [
     re_path(r'detail/(?P<id>\d+)/$',
@@ -9,6 +9,7 @@ urlpatterns = [
             name='gameplay_detail'),
     re_path(r'make_move/(?P<id>\d+)/',
             make_move,
-            name='gameplay_make_move')
+            name='gameplay_make_move'),
+    re_path(r'all$', AllGamesList.as_view())
 ]
 
